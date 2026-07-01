e790a6c26f9540a8a57873c8dc8f79b496e538a1
marienlund <marienlund@users.noreply.github.com>
2026-07-01 18:09:41 +0200

    Connect interest form to Google Apps Script

--- interest-config.js
+++ interest-config.js
@@ -1,1 +1,1 @@
-window.TROJBORG_INTEREST_ENDPOINT = "";
+window.TROJBORG_INTEREST_ENDPOINT = "https://script.google.com/macros/s/AKfycbwQgMg_r0oaDChxKb2Fu-shAWXz07_18OLu4lLxtz5HP9SetcdZzmIWDT6x1qxaz-w/exec";
