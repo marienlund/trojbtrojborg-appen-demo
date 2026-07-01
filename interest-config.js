157e55279afd0b6a6c026049961bca1ce759f27c
marienlund <marienlund@users.noreply.github.com>
2026-07-01 21:17:07 +0200

    Update interest form endpoint

--- interest-config.js
+++ interest-config.js
@@ -1,1 +1,1 @@
-window.TROJBORG_INTEREST_ENDPOINT = "https://script.google.com/macros/s/AKfycbwQgMg_r0oaDChxKb2Fu-shAWXz07_18OLu4lLxtz5HP9SetcdZzmIWDT6x1qxaz-w/exec";
+window.TROJBORG_INTEREST_ENDPOINT = "https://script.google.com/macros/s/AKfycbz7XeSSWVlZV93sOpbaHl-nwqI5cGn_-bdYY-K-z6QvfdiL7RsAc2kpnnM6D7YJDtaN/exec";
