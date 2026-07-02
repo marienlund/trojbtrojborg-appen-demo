db857fc2d328223d5b1728d512e04a180950cfdc
marienlund <marienlund@users.noreply.github.com>
2026-07-02 12:26:13 +0200

    Define sharedEndpoint variable before usage

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-  if (!sharedEndpoint) {
+const sharedEndpoint = "https://script.google.com/macros/s/AKfycbz7XeSSWVlZV93sOpbaHl-nwqI5cGn_-bdYY-K-z6QvfdiL7RsAc2kpnnM6D7YJDtaN/exec";
+
+if (!sharedEndpoint) {
     return;
   }
 
